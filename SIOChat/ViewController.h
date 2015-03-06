42e90f5261ff82c9b8caf837b5ed75b6983c70cd
mito <hkyhm5l@gmail.com>
2015-03-06 17:05:43 +0900

    Initial Commit

---
+++ SIOChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SIOChat
+//
+//  Created by YuheiMiyazato on 3/6/15.
+//  Copyright (c) 2015 mitolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SIOChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SIOChat
+//
+//  Created by YuheiMiyazato on 3/6/15.
+//  Copyright (c) 2015 mitolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
